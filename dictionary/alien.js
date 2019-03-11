88b35476c352ac6a0ef26dce15128e0dc8a4a35a
m_morikawa <slyentrance@gmail.com>
2019-03-12 08:43:59 +0900

    タグの情報を表示するようにした

--- dictionary/alien.js
+++ dictionary/alien.js
@@ -1,17 +1,23 @@
 var tagDic = '[\
-    "010000010010": {\
-        "Name": "Higgs 3",\
-        "EPC": "96 bit - 480 bit"\
-        "USER": "512 bit"\
+    {\
+        "010000010010": {\
+        "name": "Higgs 3",\
+        "epc": "96 bit - 480 bit",\
+        "user": "512 bit"\
+        }\
     },\
-    "010000010100": {\
-        "Name": "Higgs 4",\
-        "EPC": "128 bit"\
-        "USER": "128 bit"\
+    {\
+        "010000010100": {\
+            "name": "Higgs 4",\
+            "epc": "128 bit",\
+            "user": "128 bit"\
+        }\
     },\
-    "100000010001": {\
-        "Name": "Higgs EC",\
-        "EPC": "128 bit"\
-        "USER": "128 bit"\
+    {\
+        "100000010001": {\
+            "name": "Higgs EC",\
+            "epc": "128 bit",\
+            "user": "128 bit"\
+        }\
     }\
 ]';
