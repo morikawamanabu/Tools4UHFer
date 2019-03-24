9647cd69d6b1d803b1c8171c14731b890b6843ef
m_morikawa <slyentrance@gmail.com>
2019-03-24 12:01:27 +0900

    nxp.jsを更新した

--- dictionary/nxp.js
+++ dictionary/nxp.js
@@ -19,5 +19,96 @@ var tagDic = '[\
         "epc": "128 bit",\
         "user": "32 bit"\
         }\
+    },\
+    {\
+        "110100010010": {\
+        "name": "UCODE 7xm-1k",\
+        "epc": "448 bit",\
+        "user": "1-kbit"\
+        }\
+    },\
+    {\
+        "111100010010": {\
+        "name": "UCODE 7xm-2k",\
+        "epc": "448 bit",\
+        "user": "2-kbit"\
+        }\
+    },\
+    {\
+        "110110010010": {\
+        "name": "UCODE 7xm+",\
+        "epc": "448 bit",\
+        "user": "2-kbit"\
+        }\
+    },\
+    {\
+        "100000001101": {\
+        "name": "UCODE I2C (SL3S4011)",\
+        "epc": "160 bit",\
+        "user": "3328 bit"\
+        }\
+    },\
+    {\
+        "100010001101": {\
+        "name": "UCODE I2C (SL3S4021)",\
+        "epc": "160 bit",\
+        "user": "3328 bit"\
+        }\
+    },\
+    {\
+        "100000001010": {\
+        "name": "UCODE G2iM",\
+        "epc": "256 bit",\
+        "user": "512 bit"\
+        }\
+    },\
+    {\
+        "100000001011": {\
+        "name": "UCODE G2iM+",\
+        "epc": "128 bit - 448 bit",\
+        "user": "320 bit - 640 bit"\
+        }\
+    },\
+    {\
+        "100000000110": {\
+        "name": "UCODE G2iL",\
+        "epc": "128 bit",\
+        "user": "None"\
+        }\
+    },\
+    {\
+        "100100000110": {\
+        "name": "UCODE G2iL",\
+        "epc": "128 bit",\
+        "user": "None"\
+        }\
+    },\
+    {\
+        "101100000110": {\
+        "name": "UCODE G2iL",\
+        "epc": "128 bit",\
+        "user": "None"\
+        }\
+    },\
+    {\
+        "100000000111": {\
+        "name": "UCODE G2iL+",\
+        "epc": "128 bit",\
+        "user": "None"\
+        }\
+    },\
+    {\
+        "100100000111": {\
+        "name": "UCODE G2iL+",\
+        "epc": "128 bit",\
+        "user": "None"\
+        }\
+    },\
+    {\
+        "101100000111": {\
+        "name": "UCODE G2iL+",\
+        "epc": "128 bit",\
+        "user": "None"\
+        }\
     }\
 ]';
